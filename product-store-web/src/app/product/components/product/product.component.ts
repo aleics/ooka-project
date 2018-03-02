4bdb6583baed77d1163b113b1e5721e9f6bdbc41
aleics <aleics.casanovas@gmail.com>
2018-03-02 23:57:04 +0100

    add inital product view

--- product-store-web/src/app/product/components/product/product.component.ts
+++ product-store-web/src/app/product/components/product/product.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../../general/services';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../general/models';
 
 @Component({
   selector: 'ps-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.styl']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
   public isLoading = true;
+
+  private product: Product;
+
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    const productId = this.route.snapshot.params['productId'];
+    this.productsService
+      .getProduct({ id: productId })
+      .subscribe((product) => {
+        this.product = product;
+        this.isLoading = false;
+      });
+  }
 }
