3a238d3a376d846c27d27ebd548cc7c6c5a6cb9f
aleics <aleics.casanovas@gmail.com>
2018-03-02 22:53:49 +0100

    add product id

--- product-store/src/main/java/org/ooka/productstore/core/Product.java
+++ product-store/src/main/java/org/ooka/productstore/core/Product.java
@@ -2,16 +2,22 @@ package org.ooka.productstore.core;
 
 public class Product {
 
+    private final String id;
     private final String name;
     private final String description;
     private final Float price;
     private final Boolean available;
 
-    public Product(String name, String description, Float price, Boolean available) {
+    public Product(String id, String name, String description, Float price, Boolean available) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.available = available;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
