0e51c5bc3556f819882411c22a3c2dcc041d6864
aleics <aleics.casanovas@gmail.com>
2018-03-13 18:45:19 +0100

    add init component

---
+++ product-store-web/src/app/init.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ps-init',
+  templateUrl: './init.component.html',
+  styleUrls: ['./init.component.styl']
+})
+export class InitComponent {
+}
