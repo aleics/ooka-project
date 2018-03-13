40b012e3fe0dd1a442a42314e36f9431c19fca0c
Niko <nikolasglaser@leanix.net>
2018-03-13 22:20:17 +0100

    create form for login page

--- product-store-web/src/app/login/components/login/login.component.ts
+++ product-store-web/src/app/login/components/login/login.component.ts
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
+  submitted = false;
   ngOnInit() {
   }
+
+  onSubmit() { this.submitted = true; }
 
 }
