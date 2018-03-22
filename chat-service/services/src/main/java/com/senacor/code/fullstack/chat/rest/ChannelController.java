17d0a373c9cb9d958146cc80bb6553a497c6de54
aleics <aleics.casanovas@gmail.com>
2018-03-22 11:49:56 +0100

    add create channel for user

--- chat-service/services/src/main/java/com/senacor/code/fullstack/chat/rest/ChannelController.java
+++ chat-service/services/src/main/java/com/senacor/code/fullstack/chat/rest/ChannelController.java
@@ -1,12 +1,15 @@
 package com.senacor.code.fullstack.chat.rest;
 
 import com.senacor.code.fullstack.chat.domain.Channel;
+import com.senacor.code.fullstack.chat.domain.ChatMessage;
 import com.senacor.code.fullstack.chat.service.ChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -27,5 +30,10 @@ public class ChannelController {
     public List<Channel> loadChannels() {
         return channelService.loadChannels();
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Channel createChannel(
+            @Valid @RequestBody String userId
+    ) { return channelService.createChannelForUser(userId); }
 
 }
